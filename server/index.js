054017b51680129195454ae72ec0c54b4c5607f0
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-06-21 17:30:56 -0500

    Agregamos la carpeta publica para cargar archivos css y creamos vistas con pug

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
-// Importar express
+// Importar express y la carpeta de rutas
 const express = require('express');
-// 
+const path = require('path');
 const routes = require('./routes');
+
 // Configurar express
 const app = express();
+// habilitar pug
+app.set('view engine', 'pug');
+// Añadir las vistas
+app.set('views', path.join(__dirname, './views'));
+// Cargamos una carpeta estatica llamada public
+app.use(express.static('public'));
 // cacar ls rutas
 app.use('/', routes());
-
+// especificamos el puerto
 app.listen(3000);
 
